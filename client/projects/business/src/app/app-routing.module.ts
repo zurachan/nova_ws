de830c678643fe18709158aa3db37dc675493e46
zurachan <manchestervn1996@gmail.com>
2023-10-31 00:10:35 +0700

    init giao dien khach hang

--- client/projects/business/src/app/app-routing.module.ts
+++ client/projects/business/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { AboutComponent } from './page/about/about.component';
+import { ContactComponent } from './page/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/projects/business/src/app/app.module.ts
+++ client/projects/business/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { AboutComponent } from './page/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContactComponent,
+    AboutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
